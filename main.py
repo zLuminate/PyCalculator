5628be66271c2d3a604ee1cbd8f76b8551975263
zClapr <48451623+zLuminate@users.noreply.github.com>
2022-11-19 20:17:50 +0100

    extremely unsimplified

--- main.py
+++ main.py
@@ -11,19 +11,11 @@ def operate(num1, num2, op):
         return num1 - num2
 
 def calculate(expression:str):
-    # "5^2 + 3.5*2"
-
-    if " " in expression:
-        expression = expression.replace(" ", "")
+    if " " in expression: expression = expression.replace(" ", "")
+    exp_ops, exp_nums = [], []
     
-    # "5^2+3.5*2"
-    
-    exp_ops = []
-    exp_nums = []
-
     i = 0
     ops = ["^", "*", "/", "+", "-"]
-
     for count, char in enumerate(expression):
         if char in ops:
             exp_ops.append(char)
@@ -31,27 +23,43 @@ def calculate(expression:str):
             i = count + 1
         elif count == len(expression) - 1:
             exp_nums.append(expression[i:count + 1])
-    
-    # exp_ops = "^", "*", "+"
-    # exp_nums = "5", "2", "3.5", "2"
 
+    ops = ["^", "*/", "+-"]
     for op in ops:
-        while op in exp_ops:
-            num1 = float(exp_nums[exp_ops.index(op)])
-            num2 = float(exp_nums[exp_ops.index(op) + 1])
-            exp_nums[exp_ops.index(op)] = operate(num1, num2, op)
-
-            del exp_nums[exp_ops.index(op) + 1]
-            del exp_ops[exp_ops.index(op)]
+        if op == "^":
+            while "^" in exp_ops:
+                index = exp_ops.index("^")
+                exp_nums[index] = operate(int(exp_nums[index]), int(exp_nums[index + 1]), "^")
+                exp_nums.pop(index + 1)
+                exp_ops.pop(index)
+        else:
+            if (op[0] and op[1]) in exp_ops:
+                if exp_ops.index(op[0]) < exp_ops.index(op[1]):
+                    while op[0] in exp_ops:
+                        index = exp_ops.index(op[0])
+                        exp_nums[index] = operate(int(exp_nums[index]), int(exp_nums[index + 1]), op[0])
+                        exp_nums.pop(index + 1)
+                        exp_ops.pop(index)
+                else:
+                    while op[1] in exp_ops:
+                        index = exp_ops.index(op[1])
+                        exp_nums[index] = operate(int(exp_nums[index]), int(exp_nums[index + 1]), op[1])
+                        exp_nums.pop(index + 1)
+                        exp_ops.pop(index)
+            elif op[0] in exp_ops:
+                while op[0] in exp_ops:
+                    index = exp_ops.index(op[0])
+                    exp_nums[index] = operate(int(exp_nums[index]), int(exp_nums[index + 1]), op[0])
+                    exp_nums.pop(index + 1)
+                    exp_ops.pop(index)
+            elif op[1] in exp_ops:
+                while op[1] in exp_ops:
+                    index = exp_ops.index(op[1])
+                    exp_nums[index] = operate(int(exp_nums[index]), int(exp_nums[index + 1]), op[1])
+                    exp_nums.pop(index + 1)
+                    exp_ops.pop(index)
 
     return exp_nums[0]
 
 if __name__ == "__main__":
-    random_tests = [
-        ("5^2 + 3.5*2", 32),
-        ("5^2 + 3.5*2 + 2^3", 40),
-        ("5+ 4*4^2", 69)
-    ]
-
-    for test in random_tests:
-        print(calculate(test[0]) == test[1])
+    print(calculate("500*2-7^3-10+20"))
