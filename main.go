445100e287bb42d82792c5c1c20484d355f8a590
zClapr <48451623+zLuminate@users.noreply.github.com>
2022-11-20 13:12:22 +0100

    GoLang left-right BODMAS implemented

--- main.go
+++ main.go
@@ -39,13 +39,6 @@ func operate(num1 float64, num2 float64, op string) float64 {
 		return 0
 	}
 }
-func theSmallerOf(num1 float64, num2 float64) float64 {
-	if num1 < num2 {
-		return num1
-	} else {
-		return num2
-	}
-}
 func stringInSlice(a string, list []string) bool {
 	for _, b := range list {
 		if b == a {
@@ -59,23 +52,18 @@ func toString(num float64) string { return strconv.FormatFloat(num, 'f', -1, 64)
 
 func calculate(expression string) float64 {
 	expression = strings.ReplaceAll(expression, " ", "")
-	// tokens := toTokens(expression)
-	precedence := []string{"^", "*", "/", "+", "-"}
+	precedence := []string{"^", "*/", "+-"}
 	tokenized_expression := toTokens(expression)
 
 	for _, operator := range precedence {
-		// repeat until no more precedence operator in expression
-		for stringInSlice(operator, tokenized_expression) {
-			for i, token := range tokenized_expression {
-				if stringInSlice(token, strings.Split(operator, "")) {
-
-					println(strings.Join(tokenized_expression, ", "))
-
-					num1 := tokenized_expression[i-1]
-					num2 := tokenized_expression[i+1]
-					op := tokenized_expression[i]
-					result := operate(toFloat(num1), toFloat(num2), op)
-					tokenized_expression = append(tokenized_expression[:i-1], append([]string{toString(result)}, tokenized_expression[i+2:]...)...)
+		for stringInSlice(operator[0:1], tokenized_expression) || stringInSlice(operator[1:], tokenized_expression) {
+			for c, token := range tokenized_expression {
+				if strings.ContainsAny(operator, token) {
+					println(token)
+					num1 := toFloat(tokenized_expression[c-1])
+					num2 := toFloat(tokenized_expression[c+1])
+					tokenized_expression[c-1] = toString(operate(num1, num2, token))
+					tokenized_expression = append(tokenized_expression[:c], tokenized_expression[c+2:]...)
 				}
 			}
 		}
@@ -86,7 +74,6 @@ func calculate(expression string) float64 {
 
 func main() {
 	println(
-		// strings.Join(toTokens("2^3^4+1.5*1.2"), ", "),
 		calculate("2^3^4+1.5*4"),
 	)
 }
